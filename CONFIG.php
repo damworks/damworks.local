48e9d6eacd91d2f02c11c5596b47cf562f3bda32
Larry Chiarello <larry.chiarello@gmail.com>
2013-05-13 02:35:47 +0200

    v1.0 online

---
+++ CONFIG.php
@@ -0,0 +1,9 @@
+<?php
+
+//SITE GLOBAL CONFIGURATION
+$start_date = "2013/05/02"; //<-- Your site start date must be in this format yyyy/mm/dd
+$email = "larry@damworks.com"; 							//<-- Your email
+$subscribe_email = "#"; 					//<<-- Email address for sending you subscribe notification
+$subscribe_notification_subject = "New site subscriber!"; 	//<<-- Email subscribe notification subject
+
+?>
