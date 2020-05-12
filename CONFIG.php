19dcb5586131f5044040b1f0d45cf197d226057c
Larry Chiarello <lchiarello@sorelleramonda.local>
2020-05-12 09:36:44 +0200

    change email to send

--- CONFIG.php
+++ CONFIG.php
@@ -2,7 +2,7 @@
 
 //SITE GLOBAL CONFIGURATION
 $start_date = "2013/05/02"; //<-- Your site start date must be in this format yyyy/mm/dd
-$email = "larry@damworks.com"; 							//<-- Your email
+$email = "larry.chiarello@gmail.com"; 							//<-- Your email
 $subscribe_email = "#"; 					//<<-- Email address for sending you subscribe notification
 $subscribe_notification_subject = "New site subscriber!"; 	//<<-- Email subscribe notification subject
 
